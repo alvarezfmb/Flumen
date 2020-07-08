d8fc18d52ff45978eb447c47057d715e8118546f
Felipe Alvarez <felipe.alvarez@despegar.com>
2020-07-08 01:47:26 -0300

    Add bottom navigation

--- app/src/main/java/com/alvarezfmb/flumen/MainActivity.kt
+++ app/src/main/java/com/alvarezfmb/flumen/MainActivity.kt
@@ -2,12 +2,19 @@ package com.alvarezfmb.flumen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.alvarezfmb.flumen.ui.main.MainFragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.alvarezfmb.flumen.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setupWithNavController(findNavController(R.id.nav_host_fragment))
     }
 }
